7e0858e118200c10855f2d868c1f96a2baba0793
root <root@localhost.localdomain>
2015-12-18 10:02:37 -0500

    use md5sum on linux

--- goon_check/goon_check.sh
+++ goon_check/goon_check.sh
@@ -16,7 +16,7 @@ if [ ! -f $PRODUCT_ID.prev ]; then
   cp $PRODUCT_ID.curr $PRODUCT_ID.prev
 fi
 
-if [ `md5 -q $PRODUCT_ID.prev` != `md5 -q $PRODUCT_ID.curr` ]; then
+if [ `md5sum $PRODUCT_ID.prev | cut -f1 -d' '` != `md5sum $PRODUCT_ID.curr | cut -f1 -d' '` ]; then
   echo "changed"
   cp $PRODUCT_ID.curr $PRODUCT_ID.prev
   curl -s --form-string "token=adtpBkJFPSucfK2zfRo3cco2vgk9tB" --form-string "user=uiqs7S7VrF4onLHSeKwH2qKv1B4HcE" --form-string "message=`cat $PRODUCT_ID.curr`" https://api.pushover.net/1/messages.json
