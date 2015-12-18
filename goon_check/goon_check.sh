e622fa9a65c222b7ccaf9c75c5172aae6deec8c9
maverick <maverick.lou@gmail.com>
2015-12-19 00:38:01 +0800

    bug fixing for crontab

--- goon_check/goon_check.sh
+++ goon_check/goon_check.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+source /root/.profile
+
+echo ""
+echo ""
+date
+
 PRODUCT_ID=$1
-
-curl -s http://global.lotte.com/goods/viewGoodsDetail.lotte?goods_no=$PRODUCT_ID > $PRODUCT_ID.html
-
-price=`cat $PRODUCT_ID.html | pup 'div#price_div span:not([class]) text{}'`
-weight=`cat $PRODUCT_ID.html | pup 'div[class="prd-buy"] table[class="line-type table_delivery"] td:contains("g") text{}'`
-option=`cat $PRODUCT_ID.html | pup 'div[class="opt_sel"] text{}'`
-
-echo $price > $PRODUCT_ID.curr
-echo $weight >> $PRODUCT_ID.curr
-echo $option >> $PRODUCT_ID.curr
-
-if [ ! -f $PRODUCT_ID.prev ]; then
-  cp $PRODUCT_ID.curr $PRODUCT_ID.prev
+if [ ! "$PRODUCT_ID" ]; then
+  echo "product id not valid"
+  exit
 fi
 
-if [ `md5sum $PRODUCT_ID.prev | cut -f1 -d' '` != `md5sum $PRODUCT_ID.curr | cut -f1 -d' '` ]; then
+WORK_DIR=/root/code/zixuan/goon_check
+HTML_FILE=$WORK_DIR/$PRODUCT_ID.html
+PREV=$WORK_DIR/$PRODUCT_ID.prev
+CURR=$WORK_DIR/$PRODUCT_ID.curr
+
+curl -s http://global.lotte.com/goods/viewGoodsDetail.lotte?goods_no=$PRODUCT_ID > $HTML_FILE
+
+cat $HTML_FILE | /root/work/bin/pup 'div#price_div span:not([class]) text{}' > $CURR
+cat $HTML_FILE | /root/work/bin/pup 'div[class="prd-buy"] table[class="line-type table_delivery"] td:contains("g") text{}' >> $CURR
+cat $HTML_FILE | /root/work/bin/pup 'div[class="opt_sel"] text{}' >> $CURR
+
+if [ ! -f $PREV ]; then
+  cp $CURR $PREV
+fi
+
+if [ `md5sum $PREV | cut -f1 -d' '` != `md5sum $CURR | cut -f1 -d' '` ]; then
   echo "changed"
-  cp $PRODUCT_ID.curr $PRODUCT_ID.prev
-  curl -s --form-string "token=adtpBkJFPSucfK2zfRo3cco2vgk9tB" --form-string "user=uiqs7S7VrF4onLHSeKwH2qKv1B4HcE" --form-string "message=`cat $PRODUCT_ID.curr`" https://api.pushover.net/1/messages.json
+  cp $CURR $PREV
+  cat $PREV
+  curl -s --form-string "token=adtpBkJFPSucfK2zfRo3cco2vgk9tB" --form-string "user=uiqs7S7VrF4onLHSeKwH2qKv1B4HcE" --form-string "message=`cat $PREV`" https://api.pushover.net/1/messages.json
 else
   echo "not changed"
 fi
