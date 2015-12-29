cceac004ce8be842daa0a24184caa19834c530b0
maverick <maverick.lou@gmail.com>
2015-12-29 22:54:08 +0800

    add notification for delivery fee change

--- goon_check/goon_check.sh
+++ goon_check/goon_check.sh
@@ -21,9 +21,6 @@ MESSAGE_FILE=$WORK_DIR/$PRODUCT_ID.massage
 CORPID=`cat $WORK_DIR/corpid`
 CORPSECRET=`cat $WORK_DIR/corpsecret`
 
-# update delivery fee table
-curl -s -X POST -d cntry_sct_cd=CN -d frgn_dlv_co_sct_cd=01 http://global.lotte.com/cart/searchDeliverInfoListAjax.lotte | /usr/bin/jq . > $WORK_DIR/lotte_china_ems.fee
-
 curl -s http://global.lotte.com/goods/viewGoodsDetail.lotte?goods_no=$PRODUCT_ID > $HTML_FILE
 
 cat $HTML_FILE | /root/work/bin/pup 'div#price_div span:not([class]) text{}' > $CURR
@@ -41,7 +38,6 @@ function send_message()
 {
   ACCESS_TOKEN=`curl -s --form-string "corpid=$CORPID" --form-string "corpsecret=$CORPSECRET" https://qyapi.weixin.qq.com/cgi-bin/gettoken | cut -c18-103`
   curl -X POST -d '{"touser":"@all","agentid":1,"msgtype":"text","text":{"content":"'"`cat $MESSAGE_FILE`"'"}}' 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token='"$ACCESS_TOKEN"
-  cp $CURR $PREV
   curl -s --form-string "token=adtpBkJFPSucfK2zfRo3cco2vgk9tB" --form-string "user=uiqs7S7VrF4onLHSeKwH2qKv1B4HcE" --form-string "message=`cat $MESSAGE_FILE`" https://api.pushover.net/1/messages.json
 }
 
@@ -56,10 +52,33 @@ if [ `md5sum $PREV | cut -f1 -d' '` != `md5sum $CURR | cut -f1 -d' '` ]; then
     send_message
     send_message
   fi
+  cp $CURR $PREV
 else
   echo "not changed"
   if [ `cat $CURR | grep -c "XL"` -ne 0 ] ; then
     cat $CURR
     send_message
   fi
+fi
+
+
+
+
+## notification for delivery fee table changes
+curl -s -X POST -d cntry_sct_cd=CN -d frgn_dlv_co_sct_cd=01 http://global.lotte.com/cart/searchDeliverInfoListAjax.lotte | /usr/bin/jq . > $WORK_DIR/lotte_china_ems_fee.curr
+if [ ! -f $WORK_DIR/lotte_china_ems_fee.prev ]; then
+  cp $WORK_DIR/lotte_china_ems_fee.curr $WORK_DIR/lotte_china_ems_fee.prev
+fi
+
+if [ `md5sum $WORK_DIR/lotte_china_ems_fee.prev  | cut -f1 -d' '` != `md5sum $WORK_DIR/lotte_china_ems_fee.curr | cut -f1 -d' '` ]; then
+  date +"%Y-%m-%d %H:%M:%S" > $MESSAGE_FILE
+  echo "delivery fee changed" >> $MESSAGE_FILE
+  echo "previous for 10kg `cat lotte_china_ems_fee.prev | /usr/bin/jq '.[] | select(.STD_WGT_VAL == 10000) | .DLEX'`krw" >> $MESSAGE_FILE
+  echo "now      for 10kg `cat lotte_china_ems_fee.prev | /usr/bin/jq '.[] | select(.STD_WGT_VAL == 10000) | .DLEX'`krw" >> $MESSAGE_FILE
+  echo "previous for 15kg `cat lotte_china_ems_fee.prev | /usr/bin/jq '.[] | select(.STD_WGT_VAL == 15000) | .DLEX'`krw" >> $MESSAGE_FILE
+  echo "now      for 15kg `cat lotte_china_ems_fee.prev | /usr/bin/jq '.[] | select(.STD_WGT_VAL == 15000) | .DLEX'`krw" >> $MESSAGE_FILE
+  echo "previous for 20kg `cat lotte_china_ems_fee.prev | /usr/bin/jq '.[] | select(.STD_WGT_VAL == 20000) | .DLEX'`krw" >> $MESSAGE_FILE
+  echo "now      for 20kg `cat lotte_china_ems_fee.prev | /usr/bin/jq '.[] | select(.STD_WGT_VAL == 20000) | .DLEX'`krw" >> $MESSAGE_FILE
+  send_message
+  cp $WORK_DIR/lotte_china_ems_fee.curr $WORK_DIR/lotte_china_ems_fee.prev
 fi
